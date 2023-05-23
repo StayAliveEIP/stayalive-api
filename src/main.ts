1b4d5bc0442cf37721d4de4a8f923361bcfcc074
Florian <91879250+florianepitech@users.noreply.github.com>
2023-05-23 14:48:22 +0200

    add: redis connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConsoleLogger } from "@nestjs/common";
 
 async function main() {
+  const redis = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT),
+        retryAttempts: 5,
+        retryDelay: 1000,
+      },
+    },
+  );
+  await redis.listen();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 main();
--- src/validation/envValidation.ts
+++ src/validation/envValidation.ts
@@ -3,4 +3,6 @@ import * as Joi from 'joi';
 export const envValidation = Joi.object({
   MONGODB_URI: Joi.string().required(),
   MONGODB_DATABASE: Joi.string().required(),
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.number().required(),
 });
