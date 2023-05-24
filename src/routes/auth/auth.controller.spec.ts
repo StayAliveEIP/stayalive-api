0699ccc189bc134665fb27264319d8f0a046bdfb
Florian <91879250+florianepitech@users.noreply.github.com>
2023-05-24 09:33:38 +0200

    add: unit test for login and register and fix ci with database installation with dot env

--- src/routes/auth/auth.controller.spec.ts
+++ src/routes/auth/auth.controller.spec.ts
@@ -2,12 +2,34 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LoginDTO, RegisterDTO } from './auth.dto';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Rescuer, RescuerSchema } from '../../schemas/rescuer.schema';
+import { ConfigModule } from '@nestjs/config';
+import { envValidation } from '../../validation/envValidation';
 
 describe('AuthController', () => {
   let appController: AuthController;
 
+  const randomNumberEmail: number = Math.floor(Math.random() * 1000000);
+  const email: string = 'test+' + randomNumberEmail + '@test.net';
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        // Set up the environment variables.
+        ConfigModule.forRoot({
+          isGlobal: true,
+          validationSchema: envValidation,
+        }),
+        // Connect to the MongoDB database.
+        MongooseModule.forRoot(process.env.MONGODB_URI, {
+          dbName: process.env.MONGODB_DATABASE,
+        }),
+        // Get the rescuer model.
+        MongooseModule.forFeature([
+          { name: Rescuer.name, schema: RescuerSchema },
+        ]),
+      ],
       controllers: [AuthController],
       providers: [AuthService],
     }).compile();
@@ -17,7 +39,7 @@ describe('AuthController', () => {
   describe('Register the rescuer', () => {
     it('should pass the register and get the message', async () => {
       const body: RegisterDTO = {
-        email: 'test@test.net',
+        email: email,
         firstname: 'test',
         lastname: 'test',
         password: 'password123!',
@@ -32,7 +54,7 @@ describe('AuthController', () => {
     describe('Login the rescuer', () => {
       it('should pass the login and get the access token', async () => {
         const body: LoginDTO = {
-          email: 'test@test.net',
+          email: email,
           password: 'password123!',
         };
         const loginResponsePromise = await appController.login(body);
