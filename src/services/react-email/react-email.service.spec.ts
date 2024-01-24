ea49d12b9d5d1a641101ddd588b68a5865512a72
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2024-01-24 16:46:37 -0500

    fix: react email service

--- src/services/react-email/react-email.service.spec.ts
+++ src/services/react-email/react-email.service.spec.ts
@@ -1,11 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReactEmailService } from './react-email.service';
+import { ConfigModule } from '@nestjs/config';
+import { envValidation } from '../../validation/env.validation';
 
 describe('ReactEmailService', () => {
   let service: ReactEmailService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          validationSchema: envValidation,
+        }),
+      ],
+
       providers: [ReactEmailService],
     }).compile();
 
