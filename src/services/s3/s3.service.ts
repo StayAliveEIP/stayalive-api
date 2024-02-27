8c889ad6b92e73005f093ff32148580f82543f5f
TheBastosBob <bastiencantet@outlook.fr>
2024-02-26 21:07:40 -0500

    add: aws service

--- src/services/s3/s3.service.ts
+++ src/services/s3/s3.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import {DeleteObjectCommand, ListObjectsCommand, PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import {
+  DeleteObjectCommand,
+  ListObjectsCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
 
 @Injectable()
 export class AmazonS3Service {
@@ -22,42 +27,39 @@ export class AmazonS3Service {
     });
   }
 
-
-    public static getInstance(): AmazonS3Service {
-        if (!AmazonS3Service.instance) {
-          AmazonS3Service.instance = new AmazonS3Service()
-        }
-        return AmazonS3Service.instance;
-      }
-
-    public async listObjects() {
-        const command = new ListObjectsCommand({ Bucket: this.bucketName });
-        const response = await this.client.send(command);
-        return response.Contents;
-      }
-
-
-    public async uploadFile(key: string, body: any) {
-        const command = new PutObjectCommand({
-          Bucket: this.bucketName,
-          Key: key,
-          Body: body,
-          ACL: "public-read"
-        });
-        const response = await this.client.send(command);
-        return {
-          ...response,
-          url: `https://${this.bucketName}.s3.amazonaws.com/${key}`
-        }
-      }
-
-      // Method to delete a file from the bucket
-    public async deleteFile(key: string) {
-        const command = new DeleteObjectCommand({
-          Bucket: this.bucketName,
-          Key: key,
-        });
-        return await this.client.send(command);
-      }
+  public static getInstance(): AmazonS3Service {
+    if (!AmazonS3Service.instance) {
+      AmazonS3Service.instance = new AmazonS3Service();
     }
+    return AmazonS3Service.instance;
+  }
+
+  public async listObjects() {
+    const command = new ListObjectsCommand({ Bucket: this.bucketName });
+    const response = await this.client.send(command);
+    return response.Contents;
+  }
+
+  public async uploadFile(key: string, body: any) {
+    const command = new PutObjectCommand({
+      Bucket: this.bucketName,
+      Key: key,
+      Body: body,
+      ACL: 'public-read',
+    });
+    const response = await this.client.send(command);
+    return {
+      ...response,
+      url: `https://${this.bucketName}.s3.amazonaws.com/${key}`,
+    };
+  }
+
+  // Method to delete a file from the bucket
+  public async deleteFile(key: string) {
+    const command = new DeleteObjectCommand({
+      Bucket: this.bucketName,
+      Key: key,
+    });
+    return await this.client.send(command);
+  }
 }
