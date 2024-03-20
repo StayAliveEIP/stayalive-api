bf9cced2c0b55a374119ef1841242f010cc01b50
TheBastosBob <bastiencantet@outlook.fr>
2024-03-20 16:24:52 -0400

    add: chat rescuer controller

---
+++ src/routes/rescuer/chat/chat.rescuer.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Conversation } from '../../../database/conversation.schema';
+import { Types } from 'mongoose';
+import { RescuerAuthGuard } from '../../../guards/auth.route.guard';
+import { ApiResponse } from '@nestjs/swagger';
+import { ChatRescuerService } from './chat.rescuer.service';
+
+@Controller('rescuer/chat')
+export class ChatRescuerController {
+  constructor(private readonly chatService: ChatRescuerService) {}
+
+  @UseGuards(RescuerAuthGuard)
+  @Get('/conversations')
+  @ApiResponse({
+    status: 200,
+    description: 'list of conversations of the user.',
+  })
+  async getConversations(userId: Types.ObjectId): Promise<Conversation[]> {
+    return this.chatService.getConversations(userId);
+  }
+
+  @UseGuards(RescuerAuthGuard)
+  @Get('/messages')
+  @ApiResponse({
+    status: 200,
+    description: 'list of messages of the conversation.',
+  })
+  async getMessages(conversationId: Types.ObjectId): Promise<Conversation[]> {
+    return this.chatService.getMessages(conversationId);
+  }
+}
---
+++ src/routes/rescuer/chat/chat.rescuer.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Rescuer } from '../../../database/rescuer.schema';
+import { Model } from 'mongoose';
+import { ReactEmailService } from '../../../services/react-email/react-email.service';
+import { Conversation } from '../../../database/conversation.schema';
+import { Message } from '../../../database/message.schema';
+
+@Injectable()
+export class ChatRescuerService {
+  constructor(
+    @InjectModel(Rescuer.name) private rescuerModel: Model<Rescuer>,
+    @InjectModel(Conversation.name)
+    private conversationModel: Model<Conversation>,
+    @InjectModel(Message.name) private messageModel: Model<Message>,
+    private readonly reactEmailService: ReactEmailService,
+  ) {}
+}
