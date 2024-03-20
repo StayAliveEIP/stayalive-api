055f71769fc3d7a2fff2ee8fe789fc261f2138fc
TheBastosBob <bastiencantet@outlook.fr>
2024-03-20 14:37:28 -0400

    add: chat websocket skeleton

---
+++ src/websocket/chat/chat.websocket.ts
@@ -0,0 +1,74 @@
+import {
+  ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Types } from 'mongoose';
+import { Socket } from 'socket.io';
+import { Server } from 'ws';
+import * as jwt from 'jsonwebtoken';
+
+@WebSocketGateway({ namespace: '/chat/ws' })
+export class ChatWebsocket
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+  private readonly logger: Logger = new Logger(ChatWebsocket.name);
+  public rescuerClients: Map<Types.ObjectId, Socket> = new Map<
+    Types.ObjectId,
+    Socket
+  >();
+  public callCenterClients: Map<Types.ObjectId, Socket> = new Map<
+    Types.ObjectId,
+    Socket
+  >();
+
+  @WebSocketServer()
+  public server: Server;
+
+  handleConnection(@ConnectedSocket() client: Socket): any {
+    const token = client.handshake.query.token as string;
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as {
+        id: string;
+        account: string;
+      };
+      if (decoded.account !== 'rescuer' && decoded.account !== 'callCenter') {
+        client.disconnect();
+        return;
+      }
+      if (decoded.account === 'rescuer') {
+        this.rescuerClients.set(new Types.ObjectId(decoded.id), client);
+        this.logger.log(`Rescuer ${decoded.id} connected.`);
+      } else {
+        this.callCenterClients.set(new Types.ObjectId(decoded.id), client);
+        this.logger.log(`Call center ${decoded.id} connected.`);
+      }
+    } catch (e) {
+      client.disconnect();
+    }
+  }
+
+  handleDisconnect(client: any): any {
+    //search in the rescuerClients and callCenterClients maps and delete the client
+    this.rescuerClients.forEach((value, key) => {
+      if (value === client) {
+        this.rescuerClients.delete(key);
+        this.logger.log(`Rescuer ${key} disconnected.`);
+      }
+    });
+    this.callCenterClients.forEach((value, key) => {
+      if (value === client) {
+        this.callCenterClients.delete(key);
+        this.logger.log(`Call center ${key} disconnected.`);
+      }
+    });
+  }
+
+  afterInit() {
+    this.logger.log('Chat websocket initialized.');
+  }
+}
--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { RescuerWebsocket } from './rescuer/rescuer.websocket';
 import { CallCenterWebsocket } from './callCenter/callCenter.websocket';
 import { JwtStrategy } from '../guards/jwt.strategy';
+import { ChatWebsocket } from './chat/chat.websocket';
 
 @Module({
   imports: [],
-  exports: [RescuerWebsocket, CallCenterWebsocket],
-  providers: [JwtStrategy, RescuerWebsocket, CallCenterWebsocket],
+  exports: [RescuerWebsocket, CallCenterWebsocket, ChatWebsocket],
+  providers: [
+    JwtStrategy,
+    RescuerWebsocket,
+    CallCenterWebsocket,
+    ChatWebsocket,
+  ],
 })
 export class WebsocketModule {}
