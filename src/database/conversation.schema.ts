f097e72aaa311b41513f08184321d98f1846e8f0
TheBastosBob <bastiencantet@outlook.fr>
2024-03-20 15:00:04 -0400

    add: converstation schema

--- src/database/conversation.schema.ts
+++ src/database/conversation.schema.ts
@@ -1,7 +1,24 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from './document.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+export enum ConversationStatus {
+  ACTIVE = 'ACTIVE',
+  CLOSED = 'CLOSED',
+}
 
 @Schema({ versionKey: false, collection: 'conversations' })
-export class Conversation {}
+export class Conversation {
+  @Prop({ required: true })
+  callCenterId: Types.ObjectId;
+
+  @Prop({ required: true })
+  rescuerId: Types.ObjectId;
+
+  @Prop({ required: true })
+  emergencyId: Types.ObjectId;
+
+  @Prop({ required: true })
+  status: ConversationStatus;
+}
 
 export const ConversationSchema = SchemaFactory.createForClass(Conversation);
