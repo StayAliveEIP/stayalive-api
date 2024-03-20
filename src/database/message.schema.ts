b6a2584d519e38af5b4c110b4e5934a12409234a
TheBastosBob <bastiencantet@outlook.fr>
2024-03-20 15:03:00 -0400

    add: message schema

--- src/database/message.schema.ts
+++ src/database/message.schema.ts
@@ -1,7 +1,19 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Conversation } from './conversation.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
 
 @Schema({ versionKey: false, collection: 'messages' })
-export class Message {}
+export class Message {
+  @Prop({ required: true })
+  conversationId: Types.ObjectId;
+
+  @Prop({ required: true })
+  senderId: Types.ObjectId;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ required: true })
+  timestamp: Date;
+}
 
 export const MessageSchema = SchemaFactory.createForClass(Message);
