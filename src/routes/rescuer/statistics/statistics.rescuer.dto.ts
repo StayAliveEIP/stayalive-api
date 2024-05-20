c2d707f3ea448d15216ae73c44a7114a949673cb
Florian <91879250+florianepitech@users.noreply.github.com>
2024-05-20 16:30:49 +0200

    add: more success in statistics for rescuer

--- src/routes/rescuer/statistics/statistics.rescuer.dto.ts
+++ src/routes/rescuer/statistics/statistics.rescuer.dto.ts
@@ -1,11 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+export enum SuccessType {
+  EMERGENCY = 'EMERGENCY',
+  CONNECTION_TIME = 'CONNECTION_TIME',
+}
+
 export class SuccessElement {
   @ApiProperty({
     example: 'You performed a successful rescue!',
     description: 'The name of success',
   })
   name: string;
+
+  @ApiProperty({
+    example: SuccessType.EMERGENCY,
+    description: 'The type of success',
+  })
+  type: SuccessType;
 
   @ApiProperty({
     example: true,
--- src/routes/rescuer/statistics/statistics.rescuer.service.ts
+++ src/routes/rescuer/statistics/statistics.rescuer.service.ts
@@ -3,9 +3,14 @@ import { RedisService } from '../../../services/redis/redis.service';
 import { InjectModel } from '@nestjs/mongoose';
 import { Rescuer } from '../../../database/rescuer.schema';
 import { Model, Types } from 'mongoose';
-import { AvailabilityDay, SuccessElement } from './statistics.rescuer.dto';
-import { Emergency } from '../../../database/emergency.schema';
+import {
+  AvailabilityDay,
+  SuccessElement,
+  SuccessType,
+} from './statistics.rescuer.dto';
+import { Emergency, EmergencyStatus } from '../../../database/emergency.schema';
 import { AvailabilityTime } from '../../../database/availabilityTime.schema';
+import { Document } from '../../../database/document.schema';
 
 @Injectable()
 export class StatisticsRescuerService {
@@ -15,6 +20,7 @@ export class StatisticsRescuerService {
     private readonly redisService: RedisService,
     @InjectModel(Rescuer.name) private rescuerModel: Model<Rescuer>,
     @InjectModel(Emergency.name) private emergencyModel: Model<Emergency>,
+    @InjectModel(Document.name) private documentModel: Model<Document>,
     @InjectModel(AvailabilityTime.name)
     private availabilityTimeModel: Model<AvailabilityTime>,
   ) {}
@@ -63,6 +69,60 @@ export class StatisticsRescuerService {
   }
 
   async getSuccess(userId: Types.ObjectId): Promise<Array<SuccessElement>> {
-    return [];
+    const emergencySuccess = await this.getEmergencySuccess(userId);
+    const availabilitySuccess = await this.getAvailabilitySuccess(userId);
+    return [...emergencySuccess, ...availabilitySuccess];
+  }
+
+  private async getEmergencySuccess(
+    userId: Types.ObjectId,
+  ): Promise<Array<SuccessElement>> {
+    const count = [1, 10, 25, 50, 100];
+    const resolvedByRescuer = await this.emergencyModel
+      .find({ rescuerId: userId, status: EmergencyStatus.RESOLVED })
+      .countDocuments();
+    const result: Array<SuccessElement> = [];
+    for (const c of count) {
+      const isSuccess = resolvedByRescuer >= c;
+      result.push({
+        name: 'Résoudre ' + c + ' urgences',
+        isSuccessful: isSuccess,
+        type: SuccessType.EMERGENCY,
+      });
+    }
+    return result;
+  }
+
+  private async getAvailabilitySuccess(
+    userId: Types.ObjectId,
+  ): Promise<Array<SuccessElement>> {
+    const count = [7, 15, 30, 60];
+    const now = new Date();
+    const first = new Date(now.getTime() - 1000 * 60 * 60 * 24 * 60);
+    const last = new Date(now.getTime() - 1000 * 60 * 60 * 24);
+    const availabilities = await this.availabilityTimeModel.find({
+      rescuerId: userId,
+      day: {
+        $gte: first,
+        $lt: last,
+      },
+    });
+    const result: Array<SuccessElement> = [];
+    for (const c of count) {
+      const first = new Date(now.getTime() - 1000 * 60 * 60 * 24 * c);
+      const avTimeCount = availabilities
+        .filter(
+          (availability) =>
+            availability.day.getUTCDate() >= first.getUTCDate() &&
+            availability.day.getUTCDate() <= now.getUTCDate(),
+        )
+        .filter((availability) => availability.durationInSec > 0).length;
+      result.push({
+        name: `Se rendre disponible ${c} jours d'affilé`,
+        isSuccessful: avTimeCount >= c,
+        type: SuccessType.CONNECTION_TIME,
+      });
+    }
+    return result;
   }
 }
