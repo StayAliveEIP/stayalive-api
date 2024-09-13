0b70d5d0bf23d3294a4fbfc2574528c1b6e627b6
TheBastosBob <bastiencantet@outlook.fr>
2024-09-13 11:36:02 +0200

    add: new url

--- src/stategies/google.strategy.ts
+++ src/stategies/google.strategy.ts
@@ -10,7 +10,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/rescuer/auth/google/callback',
+      callbackURL: 'api.stayalive.fr:3000' + '/rescuer/auth/google/callback',
       scope: ['email', 'profile'],
     });
   }
